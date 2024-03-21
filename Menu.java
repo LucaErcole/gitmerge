4c600b70751b91221eaff41a08368b3002360c59
LucaErcole <lucahideoumeda@gmail.com>
2024-03-20 22:32:45 -0300

    segunda parte

---
+++ Menu.java
@@ -0,0 +1,7 @@
+public class Menu {
+    private void listarUsuarios() {
+        System.out.println("Menu de Opções de Listagem de Usuários:");
+        System.out.println("1. Listar todos os usuários por ordem alfabética");
+        System.out.println("2. Listar todos os usuários por data de cadastro");
+        System.out.println("3. Voltar ao menu principal");
+}
